749d7cdd447906b3593707e2db98f6719da1ac4f
André Dourado <andrew.dourado.freitas@gmail.com>
2020-11-11 17:05:41 -0300

    Utilizando SDK

--- curso-nextjs/src/pages/index.tsx
+++ curso-nextjs/src/pages/index.tsx
@@ -1,26 +1,18 @@
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import { client } from '@/lib/primisc';
+import Prismic from 'prismic-javascript';
+import { Document } from 'prismic-javascript/types/documents';
+import PrismicDOM from 'prismic-dom';
 
 import { Title } from '@/styles/pages/Home';
 import SEO from '@/components/SEO';
 
-interface IProduct {
-  id: string;
-  title: string;
-}
-
 interface HomeProps {
-  recommendedProducts: IProduct[];
+  recommendedProducts: Document[];
 }
 
 export default function Home({recommendedProducts}: HomeProps) {
-  async function handleSum() {
-    const math = (await import('@/lib/math')).default;
-
-    alert(math.sum(3, 5));
-  }
-
-  console.log(process.env.NEXT_PUBLIC_API_URL);
-
   return (
     <div>
       <SEO title="DevCommerce, your best e-commerce" image="boost.png" shouldExcludeTitleSuffix />
@@ -32,25 +24,28 @@ export default function Home({recommendedProducts}: HomeProps) {
           {recommendedProducts.map(recommendedProduct => {
             return (
               <li key={recommendedProduct.id}>
-                {recommendedProduct.title}
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                    {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
               </li>
             )
           })}
         </ul>
-
-        <button onClick={handleSum}>Somar</button>
       </section>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
-  const recommendedProducts = await response.json();
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ])
 
   return {
     props: {
-      recommendedProducts
+      recommendedProducts: recommendedProducts.results
     }
   }
 }
